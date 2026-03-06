d25c351aea2e572b14acacbe68d661b252a3ec7a
MJEKU OLTI <olti.mjeku@etu.univ-tlse2.fr>
2026-03-06 01:00:49 +0100

    Remove login method - handled by json_login firewall

--- server/src/Controller/SecurityController.php
+++ server/src/Controller/SecurityController.php
@@ -4,34 +4,10 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login', methods: ['POST'])]
-    public function login(): Response
-    {
-        // Si l'utilisateur est connecté (authentification réussie)
-        if ($this->getUser()) {
-            return $this->json([
-                'message' => 'Connexion réussie',
-                'user' => [
-                    'id' => $this->getUser()->getId(),
-                    'email' => $this->getUser()->getUserIdentifier(),
-                    'firstName' => $this->getUser()->getFirstName(),
-                    'lastName' => $this->getUser()->getLastName(),
-                    'roles' => $this->getUser()->getRoles(),
-                ]
-            ]);
-        }
-
-        // Si pas connecté après tentative → Erreur 401
-        return $this->json([
-            'error' => 'Identifiants incorrects'
-        ], 401);
-    }
-
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
